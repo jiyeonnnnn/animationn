e1e143e6448ba8216d0a14e97e7e13f9a2467773
jiyeonnnnn <93635401+jiyeonnnnn@users.noreply.github.com>
2021-11-07 17:20:43 +0900

    First

---
+++ app/src/main/java/com/google/ar/sceneform/samples/animation/main.java
@@ -0,0 +1,28 @@
+package com.google.ar.sceneform.samples.animation;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button);
+
+        Button arBtn=(Button) findViewById(R.id.ar_btn);
+        arBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
